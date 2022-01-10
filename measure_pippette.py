bc716248e0fe11c7194a2b2533086b2c62aff92b
weiern1997 <weiern1997@gmail.com>
2022-01-10 17:51:06 +0800

    Add find_liquid_level function
    Returns the estimated liquid level by finding cross section with the highest number of white pixels

--- measure_pippette.py
+++ measure_pippette.py
@@ -1,8 +1,20 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from pyimagesearch.shapedetector import ShapeDetector
 
-def find_contour1(fill):
+img = cv.imread("../sample_images/photo_2021-10-27_00-59-28.jpg")
+
+#Find all files in directory ../sample_images/ and store in an array
+def find_files(directory):
+    import os
+    files = []
+    for file in os.listdir(directory):
+        if file.endswith(".jpg"):
+            files.append(file)
+    return files
+
+def find_horizontal_edges(fill):
         """
         Given an image find a closed image that tries to eliminate non-horizontal lines
 
@@ -10,7 +22,7 @@ def find_contour1(fill):
         :return: closed: black and white image, where detected contours are in white
         """
         # get these values from the object's attributes
-        morph_dilate_kernel_size = (7, 7)
+        morph_dilate_kernel_size = (6, 6)
         morph_rect_kernel_size = (6, 1)
 
         return_image = fill
@@ -42,9 +54,9 @@ def find_contour(fill):
 
         return_image = fill
         return_image = cv.cvtColor(return_image, cv.COLOR_BGR2GRAY)
-        #threshold to binary
-        return_image = cv.adaptiveThreshold(blur,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,11,2)
+        # apply histogram equalization
+        clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+        return_image = clahe.apply(return_image)
 
         #remove noise
         kernel = np.ones((5,5),np.uint8)
@@ -79,52 +91,72 @@ def find_parameters_for_canny_edge(image, sigma=0.33):
         upper = int(min(255, (1.0 + sigma) * median))
         return lower, upper
 
+def thresh_image(fill):
+    """
+    Given an color image, converts to grayscale and applies a threshold to the image with gaussian blurring
+    """
+    blur = cv.cvtColor(fill, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(blur,(5,5),0)
+    img = cv.adaptiveThreshold(blur,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                cv.THRESH_BINARY,11,2)
+    return img
+
+def lowest_corner(fill):
+    """
+    Given an image, find the lowest corner of the pipette
+    """
+    global img
+    #Find bottom of pipette using corner detection
+    corners = cv.goodFeaturesToTrack(fill,25,0.01,10)
+    corners = np.int0(corners)
+    x, y = 0,0
+    for i in corners:
+        x1,y1 = i.ravel()
+        if y1 > y:
+                x,y = x1,y1
+    cv.circle(img,(x,y),3,255,-1)
+
+def find_liquid_level(edge_image):
+    """
+    Given image of horizontal edges.
+    Split the image into sections 3 pixels high, and find the section with the highest white pixel count and return the height of the section
+    """
+    #Split the image into sections
+    height, width = edge_image.shape
+    sections = []
+    for i in range(0,height,3):
+        sections.append(edge_image[i:i+3,:])
+    #Find the section with the highest white pixel count
+    max_white = 0
+    max_white_index = 0
+    for i in range(len(sections)):
+        white = np.sum(sections[i])
+        if white > max_white:
+            max_white = white
+            max_white_index = i
+    return max_white_index*3
+
+def main():
+
+    th3 = thresh_image(img)
+
+    lowest_corner(th3)
 
 
-img = cv.imread("..\sample_images\photo_2021-10-27_00-59-26.jpg")
+    cont,_ = cv.findContours(th3, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-blur = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-blur = cv.GaussianBlur(blur,(5,5),0)
-th3 = cv.adaptiveThreshold(blur,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv.THRESH_BINARY,11,2)
+    hor = find_horizontal_edges(img)
 
+    height = find_liquid_level(hor)
+    #find dimensions of img
+    y,x = th3.shape
+    #opencv draw a line on the image img at height=height
+    cv.line(img,(0,height),(x,height),(0,0,255),2)
 
-#Find bottom of pipette using corner detection
-corners = cv.goodFeaturesToTrack(th3,25,0.01,10)
-corners = np.int0(corners)
-x, y = 0,0
-for i in corners:
-    x1,y1 = i.ravel()
-    if y1 > y:
-            x,y = x1,y1
-cv.circle(img,(x,y),3,255,-1)
+    cv.imshow('img',img)
+    cv.imshow('th3',th3)
+    cv.imshow('hor',hor)
+    k = cv.waitKey(0)
 
-cont,_ = cv.findContours(th3, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-hor = find_contour1(img)
-y,x = th3.shape
-lines = cv.HoughLinesP(hor,1,np.pi/180,int(0.005*x),minLineLength=0.1*x,maxLineGap=0.05*x)
-if lines is not None:
-    for line in lines:
-        x1,y1,x2,y2 = line[0]
-        distance = np.sqrt((y2-y1)**2 + (x2-x1)**2)
-        angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
-        if abs(angle) < 4.5:
-            cv.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-
-
-# best = (0,1e9)
-#for i,c in enumerate(cont):
-    # ret = cv.matchShapes(c,t_cont[0],1,0.0)
-    # if ret < best[1]:
-    #cv.drawContours(img,cont,i,(255,0,0),3)
-    #best = (i,ret)
-    #print(best)
-    # cv.imshow('img',result)
-    # k=cv.waitKey(0)
-
-#cv.drawContours(img,cont,-1,(255,0,0),2)
-cv.imshow('img',img)
-cv.imshow('th3',th3)
-cv.imshow('hor',hor)
-k = cv.waitKey(0)
-
+if __name__ == '__main__':
+    main()
